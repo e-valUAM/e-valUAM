5a076b540626c7b04837bf753c3eafdd8e4060eb
pablomm <pablo.mark@hotmail.com>
2016-04-19 11:17:12 +0200

    Añadido filtro de tabla y modal de asignaturas

--- eleccionAsignaturas.php
+++ eleccionAsignaturas.php
@@ -27,58 +27,36 @@
 
 	include 'funciones.php';
 
-	$con = connect()
-	or die('No se ha podido conectar con la base de datos. Prueba de nuevo más tarde.');
-
 	session_start();
 
-	if (isset($_REQUEST['contrasenya'])) {
-
-    		$nombre = $_REQUEST['nombre'];
-	    	$contrasenya = $_REQUEST['contrasenya'];
-
-		$result =  pg_query_params($con, 'SELECT pass, id, cambio_contrasenya, profesor, admin, envio_preguntas,verificada FROM alumnos WHERE nombre =  $1', array($nombre))
-		or die('Error. Prueba de nuevo más tarde.');
-
-		$pass = pg_fetch_result($result, 0, 0);
-
-		if (crypt($contrasenya, $pass) != $pass) {
-			header("Location: ./index.php?error=si");
-   			exit;
-		}
-
-	    if (pg_fetch_result($result, 0, 6) == "f"){
-		//Caso de error
-			set_mensaje('error', 'Su cuenta no ha sido verificada, actívela siguiendo las instrucciones del mensaje que enviamos a su correo');
-			header('Location: index.php');
-			exit;
-		}
-
-
-		$_SESSION = array();
-
-		$_SESSION['nombreUsuario'] = $nombre;
-		$_SESSION['idUsuario'] = pg_fetch_result($result, 0, 1);
-
-		if (pg_fetch_result($result, 0, 2) == "t") {
-			header("Location: ./cambiarContrasenya.php");
-	   		exit;
-		}
-
-	        if (pg_fetch_result($result, 0, 3) == "t")
-        	        $_SESSION['profesor'] = True;
-
-	        if (pg_fetch_result($result, 0, 4) == "t")
-        	       	$_SESSION['admin'] = True;
-
-		$_SESSION['envio_preguntas'] = (pg_fetch_result($result, 0, 5) == 't' ? TRUE : FALSE);
-
-
-		pg_free_result($result);
-	} else if (!isset($_SESSION['idUsuario'])) {
+	//Verificacion del login
+	if (!isset($_SESSION['idUsuario'])) {
 		header("Location: ./index.php?error=si");
-                 exit;
+        exit;
 	}
+
+	//Conexion con la base de datos
+	$con = connect();
+	if ($con == null) {
+		set_mensaje('error','Error al conectar con la base de datos');
+		header("Location: ./eleccionExamen.php");
+        exit;
+	}
+
+	//Buscamos las asignaturas disponibles
+	$result =  pg_query_params($con,
+							'SELECT nombre,id,descripcion,pass,
+							id IN (SELECT id FROM alumno_por_asignaturas WHERE id_alumno = $1) AS apuntado  
+							FROM asignaturas 
+							WHERE borrada = false
+							ORDER BY nombre,id ',
+							array($_SESSION['idUsuario']))
+							or die('Error. Prueba de nuevo más tarde.');
+
+	//Caso no hay asignaturas
+	if (pg_num_rows($result) == 0) 
+		$result = null;
+
 ?>
 
 <html>
@@ -91,6 +69,10 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	   <!-- tablefilter -->
+	   <link rel="stylesheet" type="text/css" href="./scripts/TableFilter/filtergrid.css" media="screen"/>
+	   <script type="text/javascript" src="./scripts/TableFilter/tablefilter.js"></script>
+	   <script type="text/javascript" src="./scripts/TableFilter/actb.js"></script>
 	</head>
 
 	<body>
@@ -99,68 +81,52 @@
 		<main class="container-fluid">
 			<div class="row">
 				<div class="col-md-12">
-					<h1>e-valUAM, un sistema de ayuda al aprendizaje.</h1>
+					<h1>e-valUAM, inscripción a Asignaturas.</h1>
 
-					<p>El objetivo de e-valUAM es crear un modelo de estudio y de evaluación que ayude a asegurar que el alumno adquiere los conocimientos en el orden más adecuado: primero los conocimientos más básicos para construir desde ellos conocimientos más avanzados.</p>
-					<p>Para ello las pruebas que e-valUAM genera son pruebas tipo test donde cada nueva pregunta viene influida por las respuestas anteriores. Así, un alumno que responda correctamente preguntas básicas irá subiendo hacía preguntas más avanzadas.</p>
-					<p>Cada vez que un alumno accede a una prueba, estas se generan de forma aleatoria, por lo que cada vez debería acceder a un reto nuevo, hasta que sea capaz de dominar el tema.</p>
-					<p>Las pruebas no están diseñadas solo para la evaluación de cara a una nota final, sino que también se pensaron para que el alumno pueda hacer las pruebas por su cuenta de cara a realizar una auto evaluación que le permita conocer mejor cómo va desarrollando su proceso de aprendizaje.</p>
+					<p> En esta página puedes inscribirte a diversas asignaturas, es posible que algunas requieran una contraseña</p>
+					<p> Para volver a ver los exámenes disponibles pulsa <a href="./eleccionExamen.php">aquí.</a></p>
 				</div>
 			</div>
 
-		<?php
-			if (isset($_REQUEST['error']) && $_REQUEST['error'] == 'si')
-				echo "<div class=\"alert alert-danger\" id='cajon-datos' role=\"alert\">
-						<p>Se ha producido un error en el examen</p>
-					  </div>";
-		?>
+
 
 			<div class="row">
 				<div class="col-md-12">
-					<p class="lead">En la tabla siguiente encontrarás un listado de todas las pruebas disponibles actualmente. Selecciona una prueba y pulsa continuar. En ese momento empezará la prueba.</p>
-					<table class="table table-hover">
+					<p class="lead">En la tabla siguiente encontrarás un listado de todas las asignaturas disponibles actualmente.</p>
+
+
+					<table class="table table-hover" id="tablaAsignaturas" border="0"  cellspacing="0" cellpadding="0">
 						<thead>
 							<tr>
-								<th>Nombre</th>
 								<th>Asignatura</th>
-								<th>Materia</th>
-								<th>Duración</th>
-								<th>Seleccionar</th>
+								<th>Descripción</th>
+								<th>Privacidad</th>
+								<th>Opciones</th>
 							</tr>
 						</thead>
 						<tbody>
-
-							<tr>
-								<td> Cuestionario sobre hábitos en el uso de videojuegos</td>
-								<td> e-valUAM </td>
-								<td> Videojuegos </td>
-								<td> - </td>
-								<td><a class="btn btn-primary" href="gammingTest.php">Continuar</a></td>
-							</tr>
-
-
 							<?php
-								$result = pg_query_params($con, 'SELECT e.id, e.nombre, e.duracion,m.nombre AS materia,a.nombre AS asignatura 
-											FROM examenes AS e
-											INNER JOIN materias as m ON m.id = id_materia
-											INNER JOIN asignaturas AS a on id_asignatura = a.id
-											WHERE disponible = true AND comienzo < now() AND now() < comienzo + tiempo_disponible 
-											AND e.borrado = false AND a.borrada = false 
-											AND a.id IN (SELECT s.id_asignatura FROM alumno_por_asignaturas AS s WHERE s.id_alumno = $1 )
-											ORDER BY e.id', array($_SESSION['idUsuario']) )
-								or die('Error. Prueba de nuevo más tarde.');
+								//Caso no hay asignaturas
+								if($result == null){
+									echo "<tr><td>Aún no hay asignaturas para mostrar.</td><td></td><td></td><td></td></tr>";
+								}
 
 								// Imprimiendo los resultados en HTML
 								while ($line = pg_fetch_array($result, null)) {
-								    //if ($_SESSION['idUsuario'] >= 1213 && $_SESSION['idUsuario'] <= 1264 && ($line['id'] != 46 && $line['id'] != 49))
-									//continue;
 
 								    echo "\t<tr>\n";
 								    echo "\t\t<td>".$line['nombre']."</td>\n";
-								    echo "\t\t<td>".$line['asignatura']."</td>\n";
-								    echo "\t\t<td>".$line['materia']."</td>\n";
-								    echo "\t\t<td>".$line['duracion']."'</td>\n";
-								    echo "\t\t<td><a class=\"btn btn-primary\" href=\"Examen.php?idExamen=".$line['id']."\">Continuar</a></td>";
+								    echo "\t\t<td>".$line['descripcion']."</td>\n";
+
+								$icono = ($line['pass'] == null || $line['pass'] =='' ) ? "glyphicon glyphicon-eye-open" : "glyphicon glyphicon-lock";
+									echo "<td>  <span class='".$icono."'aria-hidden='true'></span></td>";
+									if($line['apuntado']='f')
+echo "\t\t<td><button type='button' class='btn btn-primary' data-toggle='modal' data-target='#myModal'>Inscribirse</button></td>";
+								    	
+
+//echo "\t\t<td><a class=\"btn btn-primary\" href=\"Examen.php?idExamen=".$line['id']."\">Inscribirse</a></td>";
+									else
+								    	echo "\t\t<td><a class=\"btn btn-primary\" href=\"Examen.php?idExamen=".$line['id']."\">Cancelar</a></td>";
 								    echo "\t</tr>\n";
 								}
 
@@ -169,10 +135,45 @@
 							?>
 						</tbody>
 					</table>
-					<p>Puedes inscribirte a nuevas asignaturas <a href="./eleccionAsignaturas.php">aquí.</a></p>
-					<p>Si quieres cambiar tu contraseña, pulsa <a href="./cambiarContrasenya.php">aquí.</a></p>
+					<script language="javascript" type="text/javascript"> 
+					//<![CDATA[ 
+					var tableFilters = {
+							col_2: "none",
+							col_3: "none",
+							paging: true,
+							paging_length: 50
+						}
+
+					setFilterGrid("tablaAsignaturas",0,tableFilters); 
+					//]]> 
+					</script> 
+
+					<p>Si quieres volver a la pagina de exámenes, pulsa <a href="./eleccionExamen.php">aquí.</a></p>
 				</div>
 			</div>
+
+			<!-- Modal Inscripcion-->
+			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+			  <div class="modal-dialog" role="document">
+				<div class="modal-content">
+				  <div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Inscripción</h4>
+				  </div>
+				  <div class="modal-body">
+					<div class="form-group">
+						<label class="control-label" for="passAsignatura">Introduzca la contraseña</label>
+						<input class="form-control" id="mensaje" type="password" name="passAsignatura" size="20" placeholder="Contraseña">
+					</div>
+				  </div>
+				  <div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+					<button type="button" class="btn btn-primary">Inscribirse</button>
+				  </div>
+				</div>
+			  </div>
+			</div>
+
 		</main>
 		<?php mostrar_licencia(); ?>
 	</body>
