351e0141a97c9fcbf349bd1cafc4c51df41aa8de
pablomm <pablo.mark@hotmail.com>
2016-06-09 14:59:08 +0200

    quitado permiso de profesor

--- nuevaCuenta.php
+++ nuevaCuenta.php
@@ -72,8 +72,11 @@
 						$token = md5(devurandom_rand() . $_POST['email'] . time());
 
 						//Inicialmente todas las cuentas tienen permiso de alumno y profesor
+
+						//Campo profesor true-false
+
 						$result =  pg_query_params($con,
-						'INSERT INTO alumnos (nombre, cambio_contrasenya, profesor, pass,token,token_creation) VALUES ($1,false,true,$2,$3,now())',
+						'INSERT INTO alumnos (nombre, cambio_contrasenya, profesor, pass,token,token_creation) VALUES ($1,false,false,$2,$3,now())',
 							array($_POST['email'],$hashed_password,$token));
 
 						//Caso de error al añadir
